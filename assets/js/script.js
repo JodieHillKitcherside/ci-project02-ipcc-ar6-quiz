9fd15c4a6f4345a64c60e587773ddd2df9e390d4
JodieHillKitcherside <>
2023-07-05 15:03:08 +0000

    Add script pages to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,64 +34,8 @@ function testKnowl() {
     quizBio.style.display = 'none';
     finalResultArea.style.display = 'none';
     quizArea.style.display = 'block';
-    showQuestions(0);
-    questionsCount(questionNumber);
-    currentScore();
+    questionCount(0);
+    avaliableQuestions = [...question];
+    currentScore(0);
     beginTimer(20);
 }
-
-// Quiz select option button
-
-function selectOption() {
-    
-}
-
-// Next question button 
-
-function nextQuestion() {
-    questionNumber++;
-    if (questionNumber, quiz.length) {
-        showQuestions(questionNumber);
-        currentScore();
-        clearInterval(timer);
-        startTimer(20);
-    }
-    else {
-        questionNumber = 0;
-        showResult();
-    }
-}
-
-// Restart quiz button 
-
-function restartQuiz() {
-
-}
-
-// Set timer to 20 secs on each question
-let counter;
-let seconds;
-let elapsedTime = 0;
-
-/**
- * The beginTimer() function restarts after each new question,
- * if no option is selected, this will display the correct answer, ??
- * and disables the selectOption(). ??
- * This then shows the nextQuestion() button. ??
- */
-
-let sec = 20;
-timer = setInterval(beginTimer, 1000);
-
-function beginTimer(time) {
-    document.getElementbyId("timer").innerHTML = sec + "sec left";
-    sec--;
-    if (sec = -1) {
-        clearInterval(time);
-        alert("Time's up! Try the next question!");
-    }
-}
-
-
-
-
