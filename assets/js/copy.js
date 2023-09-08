526cc828d02cff573d1a11ebc4876fe512f1a7db
JodieHillKitcherside <>
2023-09-08 09:30:45 +0000

    AddJSHInt checks

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -4,25 +4,35 @@ import { explanations } from "./explanations.js";
 // Constant 
 // Constant and variables declared
 const testKnowl = document.getElementById("test-knowl");
+const currentQuestion = document.getElementById("question");
+const currentQuestionIndex = document.getElementById("total-correct");
 const nextQuestion = document.getElementById("next-question");
 let currentQuestionIndex;
 let questionNumber;
 let totalCorrect;
 let questionsRemaining = quiz.length;
 
+// Global reset quiz content 
+function resetQuizContent() {
+    const quizArea = document.querySelector("#quiz-area");
+    quizArea.innerHTML = '';
+}
+
 /**
  * WINDOW ONLOAD
  * TEST KNOWL - START QUIZ
  * Event listener for test knowledge button clicked by user
  * Display none answer explanation, final result, quiz bio
+ * Show quiz area
  * Call first question
  */
 testKnowl.addEventListener("click", function () {
     document.getElementById("quiz-bio").style.display = 'none';
     document.getElementById("final-result-area").style.display = 'none';
     document.getElementById("answer-explanation").style.display = 'none';
+    document.getElementById("quiz-area").style.display = 'block';
 
-    displayQuestion(); // start quiz by calling the next question
+    displayQuestion(); // start quiz by calling the first question in loop
 });
 
 /**
@@ -34,21 +44,23 @@ testKnowl.addEventListener("click", function () {
  * 
  */
 function displayQuestion() {
-    setInterval(timerInterval);
-    const optionArea = document.querySelector("#option-select-area");ƒ
+    setInterval();
+    const optionArea = document.querySelector("#option-select-area");
     optionArea.innerHTML = '';
+  
+  	function getQuestion() {
+            var currentQuestion = document.getElementById("question-text");
+            currentQuestion.innerHTML = quiz[i].question;
+      			return currentQuestion;
+        } // sets the function to retrive and loop through each question in the quiz array
 
     for (var i = 0; i < quiz.length; i += 1) {
-        function getQuestion() {
-            var currentQuestion = document.getElementById("question-text");
-            return currentQuestion.innerHTML = quiz[i].question;
-        } // sets the function to retrive and loop through each question in the quiz array
         currentQuestion.options.forEach(option => {
             optionArea.innerHTML += `<button class="select-option">${option}</button>`;
         }); // sets the function to retrive the correct select options  and sets each option as a button
     }
+    getQuestion();
     currentQuestionIndex++;
-    timer();
 }
 
 /**
@@ -57,7 +69,8 @@ function displayQuestion() {
  * If correct +tally, +total correct and alert user
  * If incorrect option, -tally, alert user
  */
-options.addEventListener("click", function () {
+const clickedAnswer = this.innerHTML;
+clickedAnswer.addEventListener("click", function () {
     document.getElementById("quiz-area").style.display = 'block';
     const questionAnswer = currentQuestion.answer;
     const clickedAnswer = this.innerHTML;
@@ -65,10 +78,10 @@ options.addEventListener("click", function () {
 
     while (true) {
         if (questionAnswer === clickedAnswer) {
-            totalCorrect++;
-            questionNumber++;
             document.querySelector('#questions-count').innerHTML = questionsRemaining; // set the questions remaining 
             document.querySelector('#correct-count').innerHTML = totalCorrect; // set the total correct 
+            totalCorrect++;
+            questionNumber++;
             questionsRemaining--;
             revealAnswer();
             shortDelay;
@@ -81,42 +94,24 @@ options.addEventListener("click", function () {
 
 /**
  * REVEAL ANSWER
+ * Display none quiz-area, quiz bio
  * Reveal corresponding answer from questions.js
  * Reveal corresponding explanation from explanations.js
  */
+const currentAnswer = quiz[questionNumber].answer;
+const currentExplanation = explanations[questionNumber].answer;
+
+function revealExplanation() {
+    document.querySelector('.explanation').innerHTML = currentExplanation;
+}
+
 function revealAnswer() {
+    // Sort html to take answer exp out of quiz area 
+    document.getElementById("quiz-bio").style.display = 'block';
+    document.getElementById("final-result-area").style.display = 'block';
+    document.querySelector('.answer').innerHTML = currentAnswer;
     revealAnswer();
     revealExplanation();
-}
-
-
-
-
-
-const options = document.querySelectorAll("select-option"); // add the event listeners to the options
-
-options.forEach(option => option.addEventListener("click", checkAnswer)); // when the option is clicked call the check answer function
-
-
-function resetQuizContent() {
-    quizArea = document.querySelector("#quiz-area");
-    quizArea.innerHTML = '';
-}
-
-function nextQuestion() {
-    resetQuizContent(); // resets the quiz area every time 
-    clearInterval(timerInterval); // stops the timer from continuing
-    if (currentQuestionIndex < 15) {
-        // resetQuizContent();
-        // display next question from array?
-        nextQuestion = (currentQuestion + 1) % quiz.length;
-        quiz[nextQuestion];
-        document.querySelector('#question-text').innerHTML = nextQuestion.question;
-        currentQuestionIndex++;
-        timer();
-    } else {
-        finalResult();
-    }
 }
 
 /**
@@ -126,4 +121,47 @@ function nextQuestion() {
  * If yes, continue, reveal next question (questions+1)
  * If no, result takeaway
 */
+nextQuestion.addEventListener("click", function () {
+    resetQuizContent();
+   	if (questionNumber > quiz.length) {
+          questionNumber++;
+          displayQuestion();
+        }
+		else { 
+        finalResultTakeaway();
+    }
+});
 
+/**
+ * FINAL RESULT TAKEAWAY
+ * Display none quiz bio, quiz area
+ * Display final score
+ * Based on final score, reveal message
+ */
+function finalResultTakeaway() {
+    const scoreStatement = document.getElementById("score-statement");
+    document.getElementById("quiz-area").style.display = 'none';
+    document.getElementById("quiz-bio").style.display = 'none';
+    document.getElementById("result-takeaway").style.display = 'block';
+    document.getElementById("restart-quiz").style.display = 'block';
+
+    document.querySelector('#final-result').innerHTML = totalCorrect;
+
+    if (totalCorrect > 7) {
+        scoreStatement.innerHTML = "Amazing job! You scored more than 50% on the quiz.. Now it's time to put your knowledge in to action!";
+    }
+    else {
+        scoreStatement.innerHTML = "Better luck next time! You scored less than 50%, but that's really okay. We hope to have inspired you with more learnings!";
+    }
+}
+
+/**
+ * RESTART QUIZ BUTTON
+ * Bring back to quiz bio and test knowl option
+ */
+let restartQuiz = document.getElementById('restart-quiz');
+restartQuiz.addEventListener("click", function () {
+    document.getElementById("quiz-area").style.display = 'none';
+    document.getElementById("final-result-area").style.display = 'none';
+    document.getElementById("answer-explanation").style.display = 'none';
+});
